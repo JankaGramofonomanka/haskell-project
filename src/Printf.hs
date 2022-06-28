7c2872bda511da40bd271a3bdfe58bda2d0c3da3
JankaGramofonomanka <tsrn666@gmail.com>
2022-06-28 16:53:18 +0200

    Play with printf, add PyF demonstration

---
+++ src/Printf.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE
+    GADTs
+  , DataKinds
+  , KindSignatures
+  , StandaloneKindSignatures
+  , TypeApplications
+  , ScopedTypeVariables
+  , TypeFamilies
+
+  , TemplateHaskell
+  , StandaloneDeriving
+  , FlexibleInstances
+#-}
+
+module Printf where
+
+import Data.Singletons
+import Data.Singletons.TH
+import Data.List.Singletons
+import GHC.TypeLits.Singletons
+import Data.Text
+
+import Unsafe.Coerce
+
+
+$(singletons [d|
+  data Format
+    = FInt Format
+    | FString Format
+    | FOther Char Format
+    | FEnd
+  |])
+
+formatString :: String -> Format
+formatString ('%' : 'd' : cs) = FInt (formatString cs)
+formatString ('%' : 's' : cs) = FString (formatString cs)
+formatString (c : cs)        = FOther c (formatString cs)
+formatString []               = FEnd
+
+type family FormatString (s :: String) :: Format where
+  FormatString ('%' : 'd' : cs) = FInt (FormatString cs)
+  FormatString ('%' : 's' : cs) = FString (FormatString cs)
+  FormatString ( c : cs)        = FOther c (FormatString cs)
+  FormatString '[]              = FEnd
+
+sFormatString :: Sing s -> Sing (FormatString s)
+sFormatString (SCons sc1@(SChar :: SChar c1) sc2cs@(SCons (SChar :: SChar c2) scs))
+  = case (charVal (Proxy :: Proxy c1), charVal (Proxy :: Proxy c2)) of
+    ('%', 'd')  -> unsafeCoerce $ SFInt (sFormatString scs)
+    ('%', 's')  -> unsafeCoerce $ SFString (sFormatString scs)
+    (_, _)      -> unsafeCoerce $ SFOther sc1 (sFormatString sc2cs)
+
+sFormatString (SCons sc@(SChar :: SChar c) scs) = unsafeCoerce $ SFOther sc (sFormatString scs)
+sFormatString SNil = SFEnd
+  
+
+formatText :: Text -> Format
+formatText = formatString . unpack
+
+type family InterpFormat (format :: Format) :: * where
+  InterpFormat (FInt f)     = Int -> InterpFormat f
+  InterpFormat (FString f)  = String -> InterpFormat f
+  InterpFormat (FOther _ f) = InterpFormat f
+  InterpFormat FEnd         = String
+
+toFunction :: Sing f -> String -> InterpFormat f
+toFunction (SFInt sf) t       = \i -> toFunction sf (t ++ (show i))
+toFunction (SFString sf) t    = \s -> toFunction sf (t ++ s)
+toFunction (SFOther sc sf) t  = toFunction sf (t ++ [fromSing sc])
+tofunction FEnd t             = t
+
+printf :: Sing s -> InterpFormat (FormatString s)
+printf s = toFunction (sFormatString s) ""
+
+
+
---
+++ src/PyFShowcase.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module PyFShowcase where
+
+
+import PyF
+
+
+-- Ambiguous type variable
+--s0 = [fmt|{1}|]
+
+-- Ambiguous type variable, but this is an example from the tutorial
+-- s1 = [fmt|{-3:=6}|]
+
+-- unexpected ':'
+--s2 = [fmt|{1 :: Int}|]
+
+
+x, y :: Integer
+x = 1
+y = 2
+
+-- "3"
+s3 = [fmt|{x + y}|]
+
+-- "--1"
+s4 = [fmt|{x:->3}|]
+
+z :: Double
+z = 1.2345
+
+-- "1.23"
+s5 = [fmt|{z:.2}|]
+
+w :: Integer
+w = 31
+
+-- "11111"
+s6 = [fmt|{w:b}|]
+
+-- "0b11111"
+s7 = [fmt|{w:#b}|]
+
+-- "0X1F"
+s8 = [fmt|{w:#X}|]
+
+-- "1_1111_0000"
+s9 = [fmt|{16*w:_b}|]
+
+-- unexpected '{', but this is an example from the tutorial
+-- s10 = [fmt|{1086:^{2 * 10}d}|]
+
+-- "lalala"
+s11 = [fmt|{"lalala"}|]
+
+-- "--lalala--"
+s12 = [fmt|{"lalala":-^10}|]
+
+
+
+
+
+
+
